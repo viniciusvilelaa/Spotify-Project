1c83aaccc02c00ad27655240c5cf1d8933cfc1da
viniciusvilelaa <ninobez@icloud.com>
2025-02-18 19:11:32 -0300

    Alterado itemlist para exibir itens de acordo com a pagina que o usuario se encontra

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import SingleItem from './SingleItem'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
-const ItemList= ({title, items, itemArray, path, idPath}) => {
+const ItemList = ({ title, items, itemArray, path, idPath }) => {
+    const isHome = useLocation().pathname === "/"
+    const finalItems = isHome ? items : Infinity
+
     return (
-        
+
         <div className='item-list'>
             <div className='main__texts'>
                 <h2>{title} populares</h2>
-                <Link to={path} className='main__link'>Mostrar tudo</Link>
+
+                {isHome ? <Link to={path} className='main__link'>Mostrar tudo</Link> : <></>}
+                
             </div>
 
 
             <div className="item-list__container">
-                
-                {itemArray.filter((currentValue, index) => (index < items)).map((currenValue,index) => <SingleItem {...currenValue} idPath={idPath} key={`${title} - ${index}`}></SingleItem>)}
-                
-                
-            
+                {itemArray.filter((currentValue, index) => (index < finalItems)).map((currenValue, index) => <SingleItem {...currenValue} idPath={idPath} key={`${title} - ${index}`}></SingleItem>)}
+
             </div>
         </div>
     )
